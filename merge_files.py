f7ce0b422dfef09d6763836d114c9ba6f3156e34
= <jjacobs@pdx.edu>
2019-09-08 11:56:35 -0700

    finished script to read in files and output to combined file

--- merge_files.py
+++ merge_files.py
@@ -1,9 +1,12 @@
 # Program to combine multiple CSV files into a single CSV file
-import pathlib 
+import pathlib
+import csv
+import time
+
 
 def list_files_to_combine():
     # List of CSV files to return
-    csv_files = []
+    files = []
 
     # Search the input_files directory
     currentDirectory = pathlib.Path('./input_files')
@@ -13,17 +16,60 @@ def list_files_to_combine():
 
     # Read in all the CSV files from the input directory
     for file in currentDirectory.glob(file_pattern):
-        csv_files.append(file) 
+        files.append(file)
+        print(f'Adding the following file to the list of files: {file}')
 
     # Verify there are CSV files in the directory
-    if not csv_files:
+    if not files:
         raise ValueError("There must be at least one CSV file in the `input_files` folder")
 
-    print(csv_files)
+    return files
+
+
+def read_in_csv_files(files):
+    # This function reads in the data from the files
+    # and combines them into a list of dictionaries
+
+    output = []
+    for file in files:
+        with open(file, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                output.append(dict(row))
+
+    # Verify there data in the files
+    if not files:
+        raise ValueError("The CSV files were blank, there must be at least one entry")
+
+    return output
+
+
+def write_out_csv_file(data):
+    # Figure out the header names from the first entry
+    field_names = list(data[0].keys())
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    output_file_name = f'output_files\combined_output_{timestr}.csv'
+
+    # Write all the data out to a new CSV file
+    with open(output_file_name, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=field_names)
+        writer.writeheader()
+
+        for row in data:
+            writer.writerow(row)
+
+    print(f"All data output to filename: {output_file_name}")
+
 
 def main():
     print("Starting the CSV Merge Script")
-    list_files_to_combine()
+    csv_files = list_files_to_combine()
+
+    print("Read the CSV files into memory")
+    csv_data = read_in_csv_files(csv_files)
+
+    print("Write the data out to a file")
+    write_out_csv_file(csv_data)
 
 
 if __name__ == "__main__":
