9267371e9d489421deb29dbec226b72ebdb5fb49
= <jjacobs@pdx.edu>
2019-09-08 09:50:15 -0700

    adding hello world file and doing some flake8 cleanup

---
+++ hello_world.py
@@ -0,0 +1,12 @@
+
+
+# This is the main program starting point
+def main():
+    print("You ran a python program")
+
+
+# This is a convention that help if you run the file directly
+# vs importing from another python file.  Nothing to worry about
+# other than to know that this is what runs main up above.
+if __name__ == "__main__":
+    main()
--- merge_files.py
+++ merge_files.py
@@ -2,6 +2,8 @@
 
 def main():
     print("this ran")
+    print("this also ran")
+
 
 if __name__ == "__main__":
     main()
